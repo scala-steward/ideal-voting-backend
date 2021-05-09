c1df1934de321e21472c813c590a01431283abf1
Scala Steward <me@scala-steward.org>
2021-05-10 00:19:20 +0200

    Update emil-doobie to 0.9.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val doobie = "0.13.2"
     val circe = "0.13.0"
     val commonsLang = "3.12.0"
-    val emil = "0.9.1"
+    val emil = "0.9.2"
     val http4s = "0.21.22"
     val liquibase = "4.3.5"
     val logback = "1.2.3"
