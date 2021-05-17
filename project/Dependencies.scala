623f78c7ba428849564c7073f7a67ab0bf2a66b4
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-05-17 08:29:51 +0200

    Update http4s-blaze-server, http4s-circe, ... to 0.21.23 (#60)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val circe = "0.13.0"
     val commonsLang = "3.12.0"
     val emil = "0.9.2"
-    val http4s = "0.21.22"
+    val http4s = "0.21.23"
     val liquibase = "4.3.5"
     val logback = "1.2.3"
     val mariadb = "2.7.3"
