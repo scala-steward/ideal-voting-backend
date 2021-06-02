d7d5fd4a84b933faba89871297af2fadc0fd64b7
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-06-02 08:36:41 +0200

    Update zio, zio-test, zio-test-sbt to 1.0.9 (#84)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     val logback = "1.2.3"
     val mariadb = "2.7.3"
     val snakeyaml = "1.28"
-    val zio = "1.0.8"
+    val zio = "1.0.9"
     val zioCats = "2.5.1.0"
     val zioConfig = "1.0.6"
     val zioMagic = "0.3.2"
