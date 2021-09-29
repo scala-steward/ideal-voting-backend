1a3df2c51a8ad583bd28f79dc39e941b1616bf51
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-09-29 02:37:03 +0200

    Update zio-doobie-liquibase to 0.0.6 (#154)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
     val zio = "1.0.12"
     val zioCats = "2.5.1.0"
     val zioConfig = "1.0.10"
-    val zioDoobie = "0.0.5"
+    val zioDoobie = "0.0.6"
     val zioLoggingSlf4j = "0.5.12"
     val zioMagic = "0.3.9"
 
