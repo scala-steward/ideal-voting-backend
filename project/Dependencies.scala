263abbbff22e40f11bb9198f37c3a7fa4bb95b18
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-05-18 15:18:26 +0200

    Update jackson-databind to 2.13.3 (#316)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val commonsLang = "3.12.0"
     val emil = "0.12.0"
     val http4s = "0.23.11"
-    val jackson = "2.13.2.2"
+    val jackson = "2.13.3"
     val liquibaseSlf4j = "4.1.0"
     val logback = "1.2.11"
     val logbackJackson = "0.1.5"
