5f0e2e7bb6b7ba28e423da1b75fff50a9ef43d2c
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-07-15 19:04:05 +0200

    Update chimney-cats to 0.6.2 (#339)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   object Versions {
 
-    val chimney = "0.6.1"
+    val chimney = "0.6.2"
     val circe = "0.14.2"
     val commonsLang = "3.12.0"
     val emil = "0.12.0"
