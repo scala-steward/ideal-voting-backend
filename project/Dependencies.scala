885d243b5272afd5dee1e737405fcf8e70092d1e
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-09-16 20:21:43 +0200

    Update snakeyaml to 1.32 (#378)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
     val mariadb = "3.0.7"
     val monocle = "3.1.0"
     val pprint = "0.7.3"
-    val snakeyaml = "1.30"
+    val snakeyaml = "1.32"
     val zio = "2.0.2"
     val zioDoobie = "0.4.1"
     val zioLoggingSlf4j = "2.1.1"
