3f8469caf5518986b5235384484c0116aa328e5b
Scala Steward <scala_steward@virtuslab.com>
2023-07-20 16:35:48 +0000

    Update http4s-circe, http4s-client, ... to 0.23.23

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val circe = "0.14.5"
     val commonsLang = "3.12.0"
     val emil = "0.12.0"
-    val http4s = "0.23.22"
+    val http4s = "0.23.23"
     val http4sBlaze = "0.23.15"
     val jackson = "2.15.2"
     val liquibaseSlf4j = "4.1.0"
