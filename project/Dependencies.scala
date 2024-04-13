9748a9a5adbd98f6b0c24fb0ce26822a4c7fc0fc
Scala Steward <scala_steward@virtuslab.com>
2024-04-13 13:44:43 +0000

    Update zio-test, zio-test-sbt to 2.0.22

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
     val monocle = "3.2.0"
     val pprint = "0.7.3"
     val snakeyaml = "1.32"
-    val zio = "2.0.2"
+    val zio = "2.0.22"
     val zioDoobie = "0.4.1"
     val zioLoggingSlf4j = "2.1.5"
 
