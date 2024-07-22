021ab413bb5d271b8307d7501511f1f37ab7e5a5
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-07-22 16:21:46 +0200

    Update commons-lang3 to 3.15.0 (#712)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
 
     val chimney = "0.8.5"
     val circe = "0.14.7"
-    val commonsLang = "3.14.0"
+    val commonsLang = "3.15.0"
     val emil = "0.17.0"
     val http4s = "0.23.27"
     val jackson = "2.17.2"
