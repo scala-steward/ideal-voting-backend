821c84ccb5255271226e5be6544977cd3c7c39b0
Scala Steward <scala_steward@virtuslab.com>
2025-08-12 14:08:24 +0000

    Update mariadb-java-client to 3.5.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
     val logback = "1.5.18"
     val logbackJackson = "0.1.5"
     val logbackJson = "0.1.5"
-    val mariadb = "3.5.4"
+    val mariadb = "3.5.5"
     val monocle = "3.3.0"
     val pprint = "0.9.1"
     val snakeyaml = "2.3"
