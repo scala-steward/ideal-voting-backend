21e81fbc6a01bf5fb1ebf7a5d52b55e7aa7d0ba4
Scala Steward <scala_steward@virtuslab.com>
2025-09-13 15:30:14 +0000

    Update zio-config-typesafe to 4.0.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
     val pprint = "0.9.1"
     val snakeyaml = "2.3"
     val zio = "2.1.21"
-    val zioConfigTypesafe = "4.0.2"
+    val zioConfigTypesafe = "4.0.5"
     val zioLoggingSlf4j = "2.3.2"
 
     // Test
