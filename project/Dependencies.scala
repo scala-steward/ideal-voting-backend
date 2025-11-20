660b39449484b09b0f8f8f2b05d90ac2366bb985
Scala Steward <scala_steward@virtuslab.com>
2025-11-20 15:27:57 +0000

    Update pprint to 0.9.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
     val logbackJson = "0.1.5"
     val mariadb = "3.5.6"
     val monocle = "3.3.0"
-    val pprint = "0.9.1"
+    val pprint = "0.9.5"
     val snakeyaml = "2.3"
     val zio = "2.1.22"
     val zioConfigTypesafe = "4.0.2"
