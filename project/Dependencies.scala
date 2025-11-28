1226401d80c14249a83e584088a333463c47e163
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-11-28 17:27:35 +0100

    Update zio-test, zio-test-sbt to 2.1.23 (#901)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
     val monocle = "3.3.0"
     val pprint = "0.9.5"
     val snakeyaml = "2.5"
-    val zio = "2.1.22"
+    val zio = "2.1.23"
     val zioConfigTypesafe = "4.0.5"
     val zioLoggingSlf4j = "2.5.1"
 
