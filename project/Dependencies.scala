ee068de4cac747bb3cd64d424dd700a297be879d
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-12-12 19:01:47 +0100

    Update logback-classic to 1.5.22 (#909)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
     val jackson = "2.20.1"
     val liquibaseDoobie = "3.2.0"
     val liquibaseSlf4j = "5.1.0"
-    val logback = "1.5.21"
+    val logback = "1.5.22"
     val logbackJackson = "0.1.5"
     val logbackJson = "0.1.5"
     val mariadb = "3.5.6"
