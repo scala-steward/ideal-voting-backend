42c8cac1460acb7fbe7272985d8787c999b5b5df
Scala Steward <scala_steward@virtuslab.com>
2026-07-24 16:10:16 +0000

    Update logback-classic to 1.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
     val jackson = "2.22.1"
     val liquibaseDoobie = "3.2.0"
     val liquibaseSlf4j = "5.1.0"
-    val logback = "1.5.38"
+    val logback = "1.6.0"
     val logbackJackson = "0.1.5"
     val logbackJson = "0.1.5"
     val mariadb = "3.5.9"
